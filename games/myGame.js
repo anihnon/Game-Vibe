85d052b1ee0485211509af26dfb52021b6a19d8c
anihnon <asimchaprice@gmail.com>
2025-08-04 22:17:06 +0300

    Update myGame.js

--- games/myGame.js
+++ games/myGame.js
@@ -1,1 +1,12 @@
-fvwrvw
+function setup() {
+  createCanvas(400, 400);
+  background(30);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  fill(255);
+  text("ברוך הבא למשחק הדמו!", width / 2, height / 2);
+}
+
+function draw() {
+  // לא קורה כלום - רק הדגמה
+}
